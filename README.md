383b27659396a14c015b205494e7e8e87090f16c
aniruddha <aniruddhathakur76@gmail.com>
2024-10-10 12:35:57 +0530

    Fixed issue 4
    made some changes in api to receive chapters list as well also added handle chapter in /webtoon to handle chapter change and push changes into route

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ This project is an ad-free manhwa reader designed to provide a seamless reading 
 
 1. ~~After searching for a manhwa and reading a chapter, if the user hits the back button, the searched manhwa disappears, and random manhwa loads instead.~~
 2. ~~CORS issue: If the user tries to change chapters in the `/webtoon` route, they may encounter CORS errors.~~
-3. Chapter selection is not working in the `/webtoon` route.
-4. ~~If there is a " ' " in chapter name then fetch error in the `/webtoon` route (e.g. "Benefactor's Daughters")~~
-5. Chapter select is not working
+3. ~~If there is a " ' " in chapter name then fetch error in the `/webtoon` route (e.g. "Benefactor's Daughters")~~
+4. ~~Chapter selection is not working in the `/webtoon` route.~~
+5. There is no limiter for previous and next buttons in `/webtoon`
+6. Last chapter is not listed in chapter list `/webtoon`
+7. Mid chapters like `36.5` throwing 500 chapter not found error
--- src/app/webtoon/page.tsx
+++ src/app/webtoon/page.tsx
@@ -2,139 +2,181 @@
 
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { useSearchParams, useRouter } from "next/navigation"; // Use the router to manipulate URL
+import { useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import { Suspense } from "react";
 
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface Manhwa {
+  chapterPages: string[];
+  availableChapters: string[];
+}
 
 export default function Webtoon() {
-    const searchParams = useSearchParams();
-    const router = useRouter();
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-    const name = searchParams.get("name");
-    const webtoonName = name ? name.replace(/ /g, "-") : "";
-    const chapter = searchParams.get("chapter");
+  const name = searchParams.get("name");
+  const webtoonName = name ? name.replace(/ /g, "-") : "";
+  const chapter = searchParams.get("chapter");
 
-    const [searchTerm, setSearchTerm] = useState(""); // Store the input value
-    const [results, setResults] = useState<string[]>([]); // Store the API response
-    const [loadingStates, setLoadingStates] = useState<boolean[]>([]); // To track loading state for each image
-    const [loading, setLoading] = useState<boolean>(true); // General loading state
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState<Manhwa | null>(null);
+  const [loadingStates, setLoadingStates] = useState<boolean[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        if (webtoonName && chapter) {
-            const newSearchTerm = `${webtoonName} ${chapter}`;
-            setSearchTerm(newSearchTerm);
-            console.log("Constructed search term:", newSearchTerm);
+  useEffect(() => {
+    if (webtoonName && chapter) {
+      const newSearchTerm = `${webtoonName} ${chapter}`;
+      setSearchTerm(newSearchTerm);
+      console.log("Constructed search term:", newSearchTerm);
+    }
+  }, [webtoonName, chapter]);
+
+  useEffect(() => {
+    const handleSearch = async () => {
+      if (!searchTerm) return;
+
+      console.log("Calling API with search term:", searchTerm);
+
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://manhwa18-scrape-api.vercel.app/chapters?search=${searchTerm}`,
+          {
+            method: "GET",
+          }
+        );
+        const data: Manhwa = await response.json();
+        console.log("API response data:", data);
+
+        if (data && data.chapterPages) {
+          setResults(data);
+          setLoadingStates(new Array(data.chapterPages.length).fill(true));
+        } else {
+          console.error("Unexpected data structure:", data);
+          setResults(null);
+          setLoadingStates([]);
         }
-    }, [webtoonName, chapter]);
-
-    useEffect(() => {
-        const handleSearch = async () => {
-            if (!searchTerm) return;
-
-            console.log("Calling API with search term:", searchTerm);
-
-            try {
-                setLoading(true); // Start loading
-                const response = await fetch(`https://manhwa18-scrape-api.vercel.app/chapters?search=${searchTerm}`, {
-                    method: "GET",
-                });
-                const data = await response.json();
-                console.log("API response data:", data);
-
-                if (Array.isArray(data)) {
-                    setResults(data);
-                    setLoadingStates(new Array(data.length).fill(true)); // Initialize loading states
-                } else {
-                    console.error("Unexpected data structure:", data);
-                    setResults([]);
-                    setLoadingStates([]);
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-                setResults([]);
-                setLoadingStates([]);
-            } finally {
-                setLoading(false); // End loading after data is fetched
-            }
-        };
-
-        handleSearch();
-    }, [searchTerm]);
-
-    const handleImageLoad = (index: number) => {
-        setLoadingStates((prevStates) => {
-            const newStates = [...prevStates];
-            newStates[index] = false; // Set loading state to false once the image has loaded
-            return newStates;
-        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setResults(null);
+        setLoadingStates([]);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    const handleNextChapter = () => {
-        if (chapter) {
-            const nextChapter = parseInt(chapter, 10) + 1; // Increment chapter number
-            router.push(`?name=${name}&chapter=${nextChapter}`); // Update the URL
-        }
-    };
+    handleSearch();
+  }, [searchTerm]);
 
-    const handlePreviousChapter = () => {
-        if (chapter && parseInt(chapter, 10) > 1) {
-            const prevChapter = parseInt(chapter, 10) - 1; // Decrement chapter number
-            router.push(`?name=${name}&chapter=${prevChapter}`); // Update the URL
-        }
-    };
+  const handleImageLoad = (index: number) => {
+    setLoadingStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = false;
+      return newStates;
+    });
+  };
 
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <main>
-                <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 items-center">
-                    <div className="flex justify-between w-96">
-                        <Button variant="secondary" className="w-44" onClick={handlePreviousChapter}>Previous</Button>
-                        <Button variant="secondary" className="w-44" onClick={handleNextChapter}>Next</Button>
+  const handleNextChapter = () => {
+    if (chapter) {
+      const nextChapter = parseInt(chapter, 10) + 1;
+      router.push(`?name=${name}&chapter=${nextChapter}`);
+    }
+  };
+
+  const handlePreviousChapter = () => {
+    if (chapter && parseInt(chapter, 10) > 1) {
+      const prevChapter = parseInt(chapter, 10) - 1;
+      router.push(`?name=${name}&chapter=${prevChapter}`);
+    }
+  };
+
+  const handleChapterChange = (selectedChapter: string) => {
+    router.push(`?name=${name}&chapter=${selectedChapter.replace("Chapter ", "")}`);
+  };
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <main>
+        <div className="flex flex-col gap-4 py-4 justify-center items-center">
+
+          <Select onValueChange={handleChapterChange}>
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Select chapter" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Available Chapters</SelectLabel>
+                {results?.availableChapters.map((chapter, index) => (
+                  <SelectItem key={index} value={chapter}>
+                    {chapter}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <div className="flex justify-between w-64 sm:w-96">
+            <Button variant="secondary" className="w-[7rem] sm:w-44" onClick={handlePreviousChapter}>
+              Previous
+            </Button>
+            <Button variant="secondary" className="w-[7rem] sm:w-44" onClick={handleNextChapter}>
+              Next
+            </Button>
+          </div>
+
+          <div className="p-4">
+            {loading ? (
+              <div className="flex justify-center items-center">
+                <div className="spinner-border animate-spin inline-block w-12 h-12 border-4 border-t-transparent border-gray-400 rounded-full"></div>
+              </div>
+            ) : results && results.chapterPages.length > 0 ? (
+              results.chapterPages.map((url, index) => (
+                <div key={index} className="relative h-full w-full">
+                  {loadingStates[index] && (
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-transparent border-gray-400 rounded-full"></div>
                     </div>
-
-                    <div className="p-4">
-                        {loading ? (
-                            // Show a spinner while data is being fetched
-                            <div className="flex justify-center items-center">
-                                <div className="spinner-border animate-spin inline-block w-12 h-12 border-4 border-t-transparent border-gray-400 rounded-full"></div>
-                            </div>
-                        ) : (
-                            results && Array.isArray(results) && results.length > 0 ? (
-                                results.map((url, index) => (
-                                    <div key={index} className="relative h-full w-full">
-                                        {loadingStates[index] && (
-                                            <div className="absolute inset-0 flex items-center justify-center">
-                                                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-transparent border-gray-400 rounded-full"></div>
-                                            </div>
-                                        )}
-                                        <Image
-                                            width={800}
-                                            height={800}
-                                            src={url}
-                                            alt={`Chapter image ${index + 1}`}
-                                            sizes="100%"
-                                            className={`rounded object-cover ${loadingStates[index] ? 'invisible' : 'visible'}`} // Hide image until loaded
-                                            onLoadingComplete={() => handleImageLoad(index)}
-                                        />
-                                    </div>
-                                ))
-                            ) : (
-                                // Show spinners if no results but no errors occurred
-                                <div className="absolute inset-0 flex items-center justify-center">
-                                    <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-transparent border-gray-400 rounded-full"></div>
-                                </div>
-                            )
-                        )}
-                    </div>
-
-                    <div className="flex justify-between w-96">
-                        <Button variant="secondary" className="w-44" onClick={handlePreviousChapter}>Previous</Button>
-                        <Button variant="secondary" className="w-44" onClick={handleNextChapter}>Next</Button>
-                    </div>
+                  )}
+                  <Image
+                    width={800}
+                    height={800}
+                    src={url}
+                    alt={`Chapter image ${index + 1}`}
+                    sizes="100%"
+                    className={`rounded object-cover ${loadingStates[index] ? "invisible" : "visible"}`}
+                    onLoadingComplete={() => handleImageLoad(index)}
+                  />
                 </div>
-            </main>
-        </Suspense>
-    );
+              ))
+            ) : (
+              <div className="absolute inset-0 flex items-center justify-center">
+                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-transparent border-gray-400 rounded-full"></div>
+              </div>
+            )}
+          </div>
+
+          <div className="flex justify-between w-64 sm:w-96">
+            <Button variant="secondary" className="w-[7rem] sm:w-44" onClick={handlePreviousChapter}>
+              Previous
+            </Button>
+            <Button variant="secondary" className="w-[7rem] sm:w-44" onClick={handleNextChapter}>
+              Next
+            </Button>
+          </div>
+        </div>
+      </main>
+    </Suspense>
+  );
 }
