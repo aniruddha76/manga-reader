f9fb5f84c86961a75ccacdfb94bcb657a7f1421a
aniruddha <aniruddhathakur76@gmail.com>
2024-10-11 23:03:36 +0530

    Changed next chapter logic

--- src/app/webtoon/page.tsx
+++ src/app/webtoon/page.tsx
@@ -83,14 +83,15 @@ export default function Webtoon() {
   //added last chapter limiter to test
   const handleNextChapter = () => {
     const lastChapter = results?.availableChapters[0];
-
-    if (chapter && lastChapter && parseInt(chapter, 10) <= parseInt(lastChapter, 10)) {
+  
+    if (chapter && lastChapter && parseInt(chapter, 10) < parseInt(lastChapter, 10)) {
       const nextChapter = parseInt(chapter, 10) + 1;
       router.push(`?name=${name}&chapter=${nextChapter}`);
     } else {
-      router.push(`/`)
+      router.push(`/`);
     }
   };
+  
 
   const handlePreviousChapter = () => {
     if (chapter && parseInt(chapter, 10) > 1) {
